2c8cff8101b9b572e1dea9a8555b090ca9eb0396
Ralph <ralph@rgalan.es>
2017-05-03 16:23:48 +0100

    Added click event

--- pianoCSS.js
+++ pianoCSS.js
@@ -1,24 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-  function removeTransition(e) {
-    // console.log(e);
-    if (e.propertyName !== 'box-shadow') return;
 
+  function removeTransition(e) {
+    if (e.propertyName !== 'box-shadow') return;
     e.target.classList.remove('key-pressed');
   }
 
-  function keyPressed(e) {
-    const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
-    const sounds = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    sounds.currentTime = 0;
-    sounds.play();
-    key.classList.add('key-pressed');
+  function keyPlaying(e) {
+      var keyCod, key, sounds;
+
+      if( e.keyCode === undefined ) {
+        keyCode = e.target.getAttribute("data-key");
+        key = document.querySelector(`li[data-key="${keyCode}"]`);
+        sounds = document.querySelector(`audio[data-key="${keyCode}"]`);
+      } else {
+        key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+        sounds = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+      }
+
+      if (!sounds) return;
+      sounds.currentTime = 0;
+      sounds.play();
+      key.classList.add('key-pressed');
   }
 
-  const keys = Array.from(document.querySelectorAll('.key'));
+
+  var keys = Array.from(document.querySelectorAll('.key'));
 
   keys.forEach(function(element) {
-      element.addEventListener('transitionend', removeTransition)
+      element.addEventListener("click", keyPlaying);
+      element.addEventListener('transitionend', removeTransition);
   });
 
-  window.addEventListener('keydown', keyPressed);
+  window.addEventListener('keydown', keyPlaying);
 });
